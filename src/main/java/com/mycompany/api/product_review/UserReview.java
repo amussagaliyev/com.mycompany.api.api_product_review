a54817bd0c52735f79c35a49908c62d6601f1327
amussagaliyev <arman.mussagaliyev@gmail.com>
2018-08-17 00:24:02 +0300

    json validation added

--- src/main/java/com/mycompany/api/product_review/UserReview.java
+++ src/main/java/com/mycompany/api/product_review/UserReview.java
@@ -1,22 +1,39 @@
 package com.mycompany.api.product_review;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class UserReview
 {
 	@JsonProperty("productid")
+	@NotNull(message="Product was not specified")
+	@Min(value=1)
 	private Integer productId;
 
 	@JsonProperty("name")
+	@NotBlank(message="Review name cannot be null")
 	private String reviewerName;
 
 	@JsonProperty("email")
+	@NotBlank(message="Email cannot be empty")
+	@Email(message="Incorrrect email address provided")
 	private String reviewerEmailAddress;
 
 	@JsonProperty("review")
+	@NotBlank(message="Comment cannot be empty")
+	@Size(max=3850, message="Max allowed length of comment is 3850")
 	private String reviewText;
 	
 	@JsonProperty("rating")
+	@NotNull(message="Rating was not specified")
+	@Max(value=5)
+	@Min(value=0)
 	private Integer rating;
 
 	public Integer getProductId()
